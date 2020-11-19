014a518d89413908f1490a89f3796030c5e2d6e7
Caio Campos <caio.campos1204@gmail.com>
2020-11-19 18:11:40 -0300

    Atualização de Angular

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CalculatorComponent } from './calculator.component';
 
@@ -6,7 +6,7 @@ describe('CalculoComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CalculatorComponent ]
     })
